45fe76b4044f8e220b71ad1a3214a0644091fbf3
Andrew Anastasi <drewco.australia@gmail.com>
2023-11-29 12:49:35 +1100

    Working sprite mask finally

--- Dragon Dermatology/Assets/Scripts/Dragons/ScrubbableSpriteMask.cs
+++ Dragon Dermatology/Assets/Scripts/Dragons/ScrubbableSpriteMask.cs
@@ -18,13 +18,18 @@ public class ScrubbableSpriteMask : MonoBehaviour
     void Start()
     {
         Sprite dirtySprite = dirtySpriteRend.sprite;
-        Texture2D newTexture = new Texture2D(dirtySprite.texture.width, dirtySprite.texture.height);
+        // Texture2D newTexture = new Texture2D(dirtySprite.texture.width, dirtySprite.texture.height);
+        Texture2D newTexture = new Texture2D((int)dirtySprite.textureRect.width, (int)dirtySprite.textureRect.height);
         // Graphics.CopyTexture(dirtySprite.texture, newTexture);
         // Vector2 pivot = new Vector2((newTexture.width / 2), (newTexture.height / 2));
         // Vector2 pivot = Vector2.zero;
         Vector2 pivot = new Vector2(0.5f, 0.5f);
-        Sprite newMask = Sprite.Create(newTexture, dirtySprite.rect, pivot);
+        Rect newRect = new Rect(0f, 0f, newTexture.width, newTexture.height);
+        Debug.Log(gameObject.name + ": " + newRect);
+        Sprite newMask = Sprite.Create(newTexture, newRect, pivot);
+        // Sprite newMask = Sprite.Create(newTexture, dirtySprite.textureRect, pivot);
         dirtySpriteMask.sprite = newMask;
+        // GetComponent<SpriteRenderer>().sprite = newMask;// debugging
         ResetDirtyLayer();
     }
 
@@ -55,13 +60,28 @@ public class ScrubbableSpriteMask : MonoBehaviour
         dirtySpriteMask.sprite.texture.Apply(false);
     }
 
+    // private Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles)
+    // {
+    //     Vector3 dir = point - pivot; // get point direction relative to pivot
+    //     dir = Quaternion.Euler(angles) * dir; // rotate it
+    //     point = dir + pivot; // calculate rotated point
+    //     return point; // return it
+    // }
+
     public void Scrub(Vector3 worldPosition, int radius)
     {
-        Vector2 texturePos = new Vector2(worldPosition.x, worldPosition.y);
-        texturePos += (Vector2)dirtySpriteRend.sprite.bounds.extents * transform.localScale.x - (Vector2)transform.position;
-        texturePos *= dirtySpriteRend.sprite.pixelsPerUnit / transform.localScale.x;
+        // my working version no rotation
+        // Vector2 texturePos = new Vector2(worldPosition.x, worldPosition.y);
+        // texturePos += (Vector2)dirtySpriteRend.sprite.bounds.extents * transform.localScale.x - (Vector2)transform.position;
+        // texturePos *= dirtySpriteRend.sprite.pixelsPerUnit / transform.localScale.x;
 
-        //Normalize to the texture coodinates
+        // GDAU adjusted version
+        Vector2 texturePos = new Vector2(worldPosition.x, worldPosition.y);
+        texturePos = transform.InverseTransformPoint(texturePos);
+        texturePos *= dirtySpriteRend.sprite.pixelsPerUnit;// / transform.localScale.x;
+        texturePos += new Vector2(maskWidth / 2, maskHeight / 2);
+
+        // OLD! Normalize to the texture coodinates
         // int x = (int)((((worldPosition - dirtySpriteMask.transform.position).x)/dirtySpriteMaskCollider.size.x)*maskWidth/transform.localScale.x + maskWidth/2);
         // int y = (int)((((worldPosition - dirtySpriteMask.transform.position).y)/dirtySpriteMaskCollider.size.y)*maskHeight/transform.localScale.y + maskHeight/2);
 
@@ -83,6 +103,8 @@ public class ScrubbableSpriteMask : MonoBehaviour
                 nx = cx - x;
                 py = cy + y;
                 ny = cy - y;
+                // Debug.Log("Drawing x from " + nx + " to " + px);
+                // Debug.Log("Drawing y from " + ny + " to " + py);
 
                 if (nx < 0) continue;
                 if (ny < 0) continue;
@@ -90,16 +112,28 @@ public class ScrubbableSpriteMask : MonoBehaviour
                 if (py >= maskHeight) continue;
     
                 if ((py * (int)maskWidth + px) >= 0 && (py * (int)maskWidth + px) < maskColors.Length)
+                {
+                    // Debug.Log("Drawing red");
                     maskColors[py * (int)maskWidth + px] = new Color(1, 0, 0, 1);
+                }
 
                 if ((py * (int)maskWidth + nx) >= 0 && (py * (int)maskWidth + nx) < maskColors.Length)
+                {
+                    // Debug.Log("Drawing green");
                     maskColors[py * (int)maskWidth + nx] = new Color(0, 1, 0, 1);
+                }
 
                 if ((ny * (int)maskWidth + px) >= 0 && (ny * (int)maskWidth + px) < maskColors.Length)
+                {
+                    // Debug.Log("Drawing blue");
                     maskColors[ny * (int)maskWidth + px] = new Color(0, 0, 1, 1);
+                }
 
                 if ((ny * (int)maskWidth + nx) >= 0 && (ny * (int)maskWidth + nx) < maskColors.Length)
+                {
+                    // Debug.Log("Drawing white");
                     maskColors[ny * (int)maskWidth + nx] = new Color(1, 1, 1, 1);
+                }
             }
         }
     
